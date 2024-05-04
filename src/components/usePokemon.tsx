072852dfbcf1ab0f009c5997f0900eb920c90242
Lenarqa <lenar.valiev211@gmail.com>
2024-05-04 22:25:37 +0700

    useQuery

--- src/components/usePokemon.tsx
+++ src/components/usePokemon.tsx
@@ -7,36 +7,33 @@ import {
   createContext,
 } from "react";
 import { IPokemon } from "../interfaces";
+import { useQuery } from "react-query";
 
 export function usePokemonSource(): {
   pokemons: IPokemon[];
   search: string;
   setSearch: (value: string) => void;
 } {
-  // const [pokemons, setPokemons] = useState<IPokemon[]>([]);
-  // const [search, setSearch] = useState<string>("");
-
   type PokemonState = {
-    pokemons: IPokemon[];
     search: string;
   };
 
-  type PokemonAction =
-    | {
-        type: "SET_POKEMONS";
-        payload: IPokemon[];
-      }
-    | {
-        type: "SET_SEARCH";
-        payload: string;
-      };
+  type PokemonAction = {
+    type: "SET_SEARCH";
+    payload: string;
+  };
 
-  const [{ pokemons, search }, dispatch] = useReducer(
+  const { data: pokemons } = useQuery<IPokemon[]>(
+    ["pokemons"],
+    () => fetch("./pokemon.json").then((response) => response.json()),
+    {
+      initialData: [],
+    }
+  );
+
+  const [{ search }, dispatch] = useReducer(
     (state: PokemonState, action: PokemonAction) => {
       switch (action.type) {
-        case "SET_POKEMONS":
-          return { ...state, pokemons: action.payload };
-
         case "SET_SEARCH":
           return { ...state, search: action.payload };
 
@@ -45,23 +42,16 @@ export function usePokemonSource(): {
       }
     },
     {
-      pokemons: [],
       search: "",
     }
   );
-
-  useEffect(() => {
-    fetch("./pokemon.json")
-      .then((response) => response.json())
-      .then((data) => dispatch({ type: "SET_POKEMONS", payload: data }));
-  }, []);
 
   const setSearch = useCallback((value: string): void => {
     dispatch({ type: "SET_SEARCH", payload: value });
   }, []);
 
   const filteredPokemons = useMemo(() => {
-    return pokemons.filter((p) => p.name.includes(search));
+    return pokemons!.filter((p) => p.name.includes(search));
   }, [pokemons, search]);
 
   return { pokemons: filteredPokemons, search, setSearch };
